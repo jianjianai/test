615ff0684b0f5175a155e8ca95ff502f480a8875
简简爱 <2801819967@qq.com>
2024-08-02 09:14:54 +0000

    test

--- middleware.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context){
-    const { request, env } = context;
-    return new Response("hello world");
-}
-
-export const config = {
-    matcher: ['/:path*'],
-  };
