0572566465a92243363b80df85da96a48cd370db
简简爱 <2801819967@qq.com>
2024-08-02 09:09:31 +0000

    q

--- middleware.js
+++ middleware.js
@@ -2,3 +2,7 @@ export async function onRequest(context){
     const { request, env } = context;
     return new Response("hello world");
 }
+
+export const config = {
+    matcher: ['/:path*'],
+  };
